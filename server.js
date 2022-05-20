48a0be34be921760f9bf250cb93719d680047aa6
clayson frederickson <claysonshawn@gmail.com>
2022-05-20 13:36:51 -0600

    I just pushed this so I don't get fined.

--- server.js
+++ server.js
@@ -3,38 +3,37 @@ const port = 3000;
 const bodyParser = require('body-parser');//body-parder is called middleware
 const app = express();//use the library
 const md5 = require('md5');
-const redis = require('redis');
-
-const redisClient = redis.createClient();
+const {createClient} = require('redis');
+const redisClient = createClient(
+{
+        socket:{
+            port:6379,
+            host:"127.0.0.1"
+        }
+}
+);
 
 app.use(bodyParser.json());//use the middleware (call it before anything else happens on each request)
+redisClient.connect();
 
-app.listen(port, ()=>{
-    console.log("listening...")
-});
-
-app.post('/login',async (req,res)=>{//a post is when a client sends information to an API
-
-    const reqHashedPassword = md5(req.body.password)
-    const redisHashedPassword = await redisClient.hGet('password',request.body.userName);
-
-    console.log('Request Body',JSON.stringify(req.body));
-    const loginRequest = req.body;
-    //search database for username and retrieve current password
-
+app.listen(port, async ()=>{
+    console.log("listening on port: " +port);
+})
+const validatePassword = async(request, response)=>{
     
+    const requestHashedPassword = md5(request.body.password)
+    const redisHashedPassword = await redisClient.hGet('password',request.body.userName);
+    const loginRequest = request.body;
+    console.log('Request Body',JSON.stringify(request.body));
+    //search database for username and retrieve current password
     //compare the hashed version of the password that was sent with the hashed version from the database
-
-
-    if (loginRequest.userName=="rusty@mail.com" && loginRequest.password==hashedPasswordFromUser){
-        res.status(200);
-        res.send("Welcome");
+    if (requestHashedPassword==redisHashedPassword){
+        response.status(200);
+        response.send("Welcome");
     } else{
-        res.status(401);
-        res.send("Unauthorized");
+        response.status(401);
+        response.send("Unauthorized");
         }
-});
+};
 
-app.get('/',(req,res)=>{//every time something calls your API, that is a request
-    res.send("HOLY CRAP ITS WORKING!")//a response is when an API gives the inofrmation requested
-});
+app.post('/login', validatePassword);
