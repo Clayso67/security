1e366167be08733b305082fa697ebccd84cd1d0e
clayson frederickson <claysonshawn@gmail.com>
2022-05-27 16:00:06 -0600

    changes to signup and sll stuff'

--- apiclient.bat
+++ apiclient.bat
@@ -1,1 +1,1 @@
-curl -d "@data.json" -X POST http://localhost:3000/login -H "content-Type:application/json"
+curl -d "@data.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
